aa6078ef24c670a25afd8780c17f41d6e0329b2c
MattBussing <mbussing@mymail.mines.edu>
2019-05-17 13:32:57 -0600

    started making the database for DrawingModel

--- models/drawings.py
+++ models/drawings.py
@@ -1,3 +1,4 @@
+import hashlib
 from datetime import datetime
 
 import pytz
@@ -45,38 +46,39 @@ class DrawingModel(db.Model):
                        db.ForeignKey('people.person')
                        )
 
-    # def __init__(self, recipient, message):
-    #     if PeopleModel.find_person(recipient):
-    #         self.recipient = recipient
-    #         self.message = message
-    #         self.date_time = datetime.now(tz=pytz.utc)
-    #     else:
-    #         raise Exception("Person does not exist")
-    #
-    # def json(self):
-    #     return {
-    #         'recipient': self.recipient,
-    #         'message': self.message,
-    #         'dateTime': self.date_time.isoformat()
-    #     }
-    #
-    # def save_to_db(self):
-    #     db.session.add(self)
-    #     db.session.commit()
-    #
-    # def delete_from_db(self):
-    #     db.session.delete(self)
-    #     db.session.commit()
-    #
-    # @classmethod
-    # def search_for_message(cls, message, recipient):
-    #     return cls.query.filter_by(message=message,
-    #                                recipient=recipient).first()
-    #
-    # @classmethod
-    # def find_all_by_recipient(cls, recipient):
-    #     return cls.query.filter_by(recipient=recipient).all()
-    #
-    # @classmethod
-    # def find_all(cls):
-    #     return cls.query.order_by(cls.recipient).all()
+    def __init__(self, drawing, recipient, sender):
+        # print
+        hash_object = hashlib.sha256(drawing)
+        hex_dig = hash_object.hexdigest()
+        print(hex_dig)
+        # TODO finish , get recipient and sender
+        if PeopleModel.find_person(recipient) \
+                and PeopleModel.find_person(sender):
+            self.id = hex_dig
+            self.drawing = drawing
+            self.date_time = datetime.now(tz=pytz.utc)
+            self.recipient = recipient
+            self.sender = sender
+        else:
+            raise Exception("Sender or Recipient does not exist")
+
+    def json(self):
+        return {
+            'id': self.id,
+            'drawing': self.drawing,
+            'date_time': self.date_time.isoformat(),
+            'recipient': self.recipient,
+            'sender': self.sender
+        }
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def search_for_drawing_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
